b09f235a32f6d2415302ab98ba90a02b1a444736
manucrack0510 <manuel.defreitasmirabal@rochester.edu.co>
2026-05-27 13:14:59 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+/** 
+Ideas:
+ a haunted library with ghosts in which the player collect books and have messages talking about the theory of election, the sostenibility, and how to learn something. After collecting the books, it will make a little quiz about the topics shown in the books.
+
+ */
+tiles.setCurrentTilemap(tilemap`level1`)
